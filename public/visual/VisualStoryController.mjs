656cacff7d7f8cde3a9d23568836f852dbd18fb8
wertikooo-web <wertikooo@gmail.com>
2026-08-07 14:10:05 +0300

    Hydrate starter settings from server

--- public/visual/VisualStoryController.mjs
+++ public/visual/VisualStoryController.mjs
@@ -1,6 +1,8 @@
 import { mountStartIntentLauncher } from '/avatar-modules/StartIntentLauncher.mjs';
 import { mountStartIntentSettings } from '/avatar-modules/StartIntentSettings.mjs';
+import { createServerBackedStartIntentStorage } from '/avatar-modules/StartIntentPersistence.mjs';
 export * from '/avatar-modules/VisualStoryControllerCore.mjs';
 
-mountStartIntentLauncher();
-mountStartIntentSettings();
+const startIntentStorage = await createServerBackedStartIntentStorage();
+mountStartIntentLauncher({ storage: startIntentStorage });
+mountStartIntentSettings({ storage: startIntentStorage });
